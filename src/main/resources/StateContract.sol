3c22d9c1c6ac78ebd0eb33861fb85a52ce3fc4df
pushparajk <pushparaj.kamaraj@gmail.com>
2020-11-05 16:28:52 +0530

    Modified for more methods

--- src/main/resources/StateContract.sol
+++ src/main/resources/StateContract.sol
@@ -13,16 +13,40 @@ contract StateContract {
 	
 	uint balanceAmount;
 	
+	//new string[](size);
+	
 	constructor(string memory _schemeName, uint _schemeSanctionedAmount, string memory _centralContractAddress,uint _stateId) public {
         schemeName = _schemeName;
         schemeSanctionedAmount = _schemeSanctionedAmount;
 		centralContractAddress=_centralContractAddress;
 		stateId = _stateId;
-		balanceAmount=schemeSanctionedAmount;
-    }
+		balanceAmount=_schemeSanctionedAmount;
+	}
 
 	function disburseAmountToIndividual(uint _amount) public {
 		balanceAmount -=_amount;
 	}
+	
+	function getBalanceAmount() public view returns(uint _balanceAmount){
+		return balanceAmount;
+	}	
+
+	function getSchemeName() public view returns(string memory){
+		return schemeName;
+	}	
+
+	function getSchemeSanctionedAmount() public view returns(uint sAmount){
+		return schemeSanctionedAmount;
+	}	
+
+	function getcentralContractAddress() public view returns(string memory sAmount){
+		return centralContractAddress;
+	}	
+
+
+	function getStateId() public view returns(uint sStateId){
+		return stateId;
+	}	
+
 	
 }
