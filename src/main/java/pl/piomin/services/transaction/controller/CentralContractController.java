4879dda8caa12fe405d0334d0b21ca03c586a897
pushparajk <pushparaj.kamaraj@gmail.com>
2020-11-05 16:30:55 +0530

    Added more methods

--- src/main/java/pl/piomin/services/transaction/controller/CentralContractController.java
+++ src/main/java/pl/piomin/services/transaction/controller/CentralContractController.java
@@ -28,6 +28,7 @@ import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
 
 import pl.piomin.services.transaction.model.Contract;
 import pl.piomin.services.transaction.model.DisbursementModel;
+import pl.piomin.services.transaction.model.FundAllocationModel;
 import pl.piomin.services.transaction.services.CentralSchemeService;
 
 import org.slf4j.Logger;
@@ -42,42 +43,17 @@ public class CentralContractController {
     private static final Logger LOGGER = LoggerFactory.getLogger(CentralContractController.class);
     public static final BigInteger GAS_PRICE = BigInteger.valueOf(1L);
     public static final BigInteger GAS_LIMIT = BigInteger.valueOf(21_000L);
-/*    
-    //@Value("${contract-service.url}")
-    //String url;
-    @Autowired
-    Web3j web3j;
-    @Autowired
-    RestTemplate template;
-    Credentials credentials;
-    
-    @PostConstruct
-    public void init() throws IOException, CipherException, NoSuchAlgorithmException, NoSuchProviderException, InvalidAlgorithmParameterException {    	
-    	EthCoinbase coinbase = web3j.ethCoinbase().send();
-    	EthAccounts accounts = web3j.ethAccounts().send();
-    	for (int i = 1; i < accounts.getAccounts().size(); i++) {
-    		EthGetTransactionCount transactionCount = web3j.ethGetTransactionCount(coinbase.getAddress(), DefaultBlockParameterName.LATEST).send();
-            Transaction transaction = Transaction.createEtherTransaction(coinbase.getAddress(), transactionCount.getTransactionCount(), GAS_PRICE, GAS_LIMIT, accounts.getAccounts().get(i), BigInteger.valueOf(25_000_000_000L));
-            web3j.ethSendTransaction(transaction).send();
-            EthGetBalance balance = web3j.ethGetBalance(accounts.getAccounts().get(i), DefaultBlockParameterName.LATEST).send();
-        	LOGGER.info("Balance: address={}, amount={}", accounts.getAccounts().get(i), balance.getBalance().longValue());
-		}
-    }
-*/
 
     @Autowired
     CentralSchemeService service;
     
-///centralScheme/schemeName
     @GetMapping(value="/schemename/{address}")
-    //@RequestMapping("{address}")
     public String getSchemeName(@PathVariable String address) {
     	System.out.println("Inside getSchemeName() in controller address = "+address);
     	return service.getSchemeName(address);
     }
 
     @GetMapping(value="/schemeamount/{address}")
-    //@RequestMapping("{address}")
     public String getSchemeAmount(@PathVariable String address) {
     	System.out.println("Inside getSchemeAmount() in controller");
     	return ""+service.getSchemeAmount(address);
@@ -89,37 +65,50 @@ public class CentralContractController {
     	return service.createCentralScheme(newContract);
     }
 
+    @GetMapping(value="/getCentralSchemeDetails")
+    //@RequestMapping("{address}")
+    public Contract getCentralSchemeDetails(@RequestBody Contract ContractModel) {
+    	System.out.println("Inside getSchemeAmount() in controller");
+    	return service.getCentralSchemeDetails(ContractModel);
+    }
+
+    
     @PostMapping(path = "/disburseAmountToState")
-    public DisbursementModel disburseAmountToState(@RequestBody DisbursementModel newDisbursementModel) throws Exception {
+    public FundAllocationModel disburseAmountToState(@RequestBody FundAllocationModel newDisbursementModel) throws Exception {
     	System.out.println("Inside createContract()");
     	return service.disburseAmountToState(newDisbursementModel);
     }
 
-    @GetMapping(value="/getDisbursementAmount")
-    //@RequestMapping("{address}")
-    public DisbursementModel getDisbursementAmount(@RequestBody DisbursementModel newDisbursementModel) {
-    	System.out.println("Inside getSchemeAmount() in controller");
-    	return service.getdisburseAmountToState(newDisbursementModel);
-    }
     
     @PostMapping(path = "/reverseBalanceAmountFromState")
-    public DisbursementModel reverseBalanceAmountFromState(@RequestBody DisbursementModel newDisbursementModel) throws Exception {
+    public FundAllocationModel reverseBalanceAmountFromState(@RequestBody FundAllocationModel newDisbursementModel) throws Exception {
     	System.out.println("Inside reverseBalanceAmountFromState() in controller");
     	return service.reverseBalanceAmountFromState(newDisbursementModel);
     }
 
 
     @GetMapping(value="/getReverseBalanceAmountFromState")
-    //@RequestMapping("{address}")
-    public DisbursementModel getReverseBalanceAmountFromState(@RequestBody DisbursementModel newDisbursementModel) {
+    public FundAllocationModel getReverseBalanceAmountFromState(@RequestBody FundAllocationModel newDisbursementModel) {
     	System.out.println("Inside getDisbursementAmount() in controller");
     	return service.getReverseBalanceAmountFromState(newDisbursementModel);
     }
 
+    @GetMapping(value="/getStateSchemeDetails")
+    public FundAllocationModel getStateSchemeDetails(@RequestBody FundAllocationModel newFundAllocationModel) {
+    	System.out.println("Inside getDisbursementAmount() in controller");
+    	return service.getStateSchemeDetails(newFundAllocationModel);
+    }
+    
     @PostMapping(path = "/disburseAmountToIndividual")
     public DisbursementModel disburseAmountToIndividual(@RequestBody DisbursementModel newDisbursementModel) throws Exception {
     	System.out.println("Inside disburseAmountToIndividual() in controller");
     	return service.disburseAmountToIndividual(newDisbursementModel);
+    }
+
+    @GetMapping(value="/getDisbursementDetails")
+    public DisbursementModel getDisbursementDetails(@RequestBody DisbursementModel newDisbursementModel) {
+    	System.out.println("Inside getDisbursementAmount() in controller");
+    	return service.getDisbursementDetails(newDisbursementModel);
     }
 
 }
