9e98a634dc689495b2879eda9bbaa74c7137d87f
pushparajk <pushparaj.kamaraj@gmail.com>
2020-11-09 16:21:21 +0530

    Latest changes to solidity files
    DonationContract newly added. State and central contract updated

--- src/main/resources/StateContract.sol
+++ src/main/resources/StateContract.sol
@@ -2,9 +2,6 @@ pragma solidity >=0.4.6;
 
 contract StateContract {
 
-    // (1)
-    string schemeName;
-    // (2)
     uint schemeSanctionedAmount;
 	
 	string centralContractAddress;
@@ -15,8 +12,7 @@ contract StateContract {
 	
 	//new string[](size);
 	
-	constructor(string memory _schemeName, uint _schemeSanctionedAmount, string memory _centralContractAddress,uint _stateId) public {
-        schemeName = _schemeName;
+	constructor( uint _schemeSanctionedAmount, string memory _centralContractAddress,uint _stateId) public {
         schemeSanctionedAmount = _schemeSanctionedAmount;
 		centralContractAddress=_centralContractAddress;
 		stateId = _stateId;
@@ -27,13 +23,12 @@ contract StateContract {
 		balanceAmount -=_amount;
 	}
 	
+
+
 	function getBalanceAmount() public view returns(uint _balanceAmount){
 		return balanceAmount;
 	}	
 
-	function getSchemeName() public view returns(string memory){
-		return schemeName;
-	}	
 
 	function getSchemeSanctionedAmount() public view returns(uint sAmount){
 		return schemeSanctionedAmount;
