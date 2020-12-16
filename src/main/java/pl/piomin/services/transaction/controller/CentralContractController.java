fb16eb3d77cc468cde50474b5e7f802a9f32d72a
K <Pushparaj.K@finastra.com>
2020-12-16 15:12:06 +0530

    @CrossOrigin added in constroller

--- src/main/java/pl/piomin/services/transaction/controller/CentralContractController.java
+++ src/main/java/pl/piomin/services/transaction/controller/CentralContractController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +25,7 @@ import pl.piomin.services.transaction.model.StateFundAllocation;
 import pl.piomin.services.transaction.services.CentralSchemeService;
 
 
-
+@CrossOrigin 
 @RestController
 @RequestMapping("/scheme")
 public class CentralContractController {
